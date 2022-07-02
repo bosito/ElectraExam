0e409d22558ece7944a516185a27c1e61c4883d6
Jose Luis <bosito321@gmail.xom>
2022-07-01 21:51:09 -0500

    se agregaron mas dependencias

--- src/app/screens/calendar/CalendarPage.tsx
+++ src/app/screens/calendar/CalendarPage.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Calendar, globalizeLocalizer } from 'react-big-calendar'
 
 export default function CalendarPage() {
   return (
     <div className='' >
-      
+      {/* <Calendar
+        localizer={{  }}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      /> */}
     </div>
   )
 }
